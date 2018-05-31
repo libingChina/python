152b88d29b81abb460bd7011e4f7ec57a979117e
李炳 <libing1174365569@163.com>
2018-05-31 21:16:25 +0800

    开始项目

---
+++ screen.py
@@ -0,0 +1,20 @@
+from PIL import ImageGrab
+import socket
+import time
+a=time.time()
+img=ImageGrab.grab()
+img.save('123.jpeg')
+f=open('123.jpeg','rb')
+udp= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+udp.bind(('',8888))
+while True:
+    f1=f.read(1024)
+    if f1:
+        udp.sendto(f1,('192.168.199.255',8889))
+    else:
+        udp.sendto(''.encode(), ('192.168.199.255', 8889))
+        print('~!@~!@'.encode())
+        break
+udp.close()
+print(time.time()-a)
